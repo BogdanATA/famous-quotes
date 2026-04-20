12a27cb9b5a9b34e2bbacd40e205cb6fd3dc7e4c
BogdanATA <batameev@gmail.com>
2026-04-20 10:36:16 -0400

    - created quotes array and initialized 10 quotes

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FamousQuotes {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //list of quotes
+        String[] quotes = new String[10];
+
+        quotes[0] = "Action beats intention.";
+        quotes[1] = "Small steps compound.";
+        quotes[2] = "Clarity creates momentum.";
+        quotes[3] = "Discipline over motivation.";
+        quotes[4] = "Focus on what matters.";
+        quotes[5] = "Consistency builds results.";
+        quotes[6] = "Done is better than perfect.";
+        quotes[7] = "Learn, adapt, repeat.";
+        quotes[8] = "Time reveals truth.";
+        quotes[9] = "Start before you’re ready.";
+
+
+
+    }
 }
